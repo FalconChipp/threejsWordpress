501dd48490589259ae6c0255da9c24175255b6b2
Ryan <112320693+FalconChipp@users.noreply.github.com>
2023-05-17 16:02:22 +0100

    Convert how models are created and handled

--- admin/class-threejs-model-viewer-admin.php
+++ admin/class-threejs-model-viewer-admin.php
@@ -6,14 +6,56 @@ class ThreeJS_Model_Viewer_Admin {
     public function __construct($plugin_slug, $version) {
         $this->plugin_slug = $plugin_slug;
         $this->version = $version;
+        
+        add_action('admin_init', array($this, 'register_settings'));
     }
+    
+    public function register_settings() {
+        // Initialize the 'threejs_models' option if it doesn't exist
+        if (false === get_option('threejs_models')) {
+            add_option('threejs_models', array());
+        }
+
+        register_setting('threejs-model-viewer-settings', 'threejs_models'); 
+    
+        add_settings_section(
+            'threejs_model_manager_section',
+            'ThreeJS Model Manager',
+            array($this, 'model_manager_section_callback'),
+            'threejs-model-viewer-settings'
+        );
+    
+        add_settings_field(
+            'threejs_model',
+            '3D Model',
+            array($this, 'threejs_model_render'),
+            'threejs-model-viewer-settings',
+            'threejs_model_manager_section'
+        );
+    }
+
+    public function model_manager_section_callback() {
+        echo '<p>Add and manage your 3D Models here</p>';
+    }
+
+    public function threejs_model_render() {
+        $options = get_option('threejs_models');
+        $model = isset($options['threejs_model']) ? $options['threejs_model'] : '';
+        ?>
+            <input type='text' name='threejs_models[threejs_model]' value='<?php echo $model; ?>'>
+        <?php
+    }
+
+
     public function enqueue_styles() {
         wp_enqueue_style($this->plugin_slug, plugin_dir_url(__FILE__) . 'css/admin.css', array(), $this->version, 'all');
     }
+
     public function enqueue_scripts() {
         wp_enqueue_script($this->plugin_slug, plugin_dir_url(__FILE__) . 'js/admin.js', array('jquery'), $this->version, false);
         wp_enqueue_script('three-js', 'https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js', array(), null, true);
     }
+
     public function add_admin_menu() {
         add_menu_page(
             'ThreeJS Model Manager',
@@ -25,88 +67,28 @@ class ThreeJS_Model_Viewer_Admin {
             20
         );
     }
+
     public function display_model_manager_page() {
         if (!current_user_can('manage_options')) {
             return;
         }
-
+    
         echo '<div class="wrap">';
         echo '<h1>' . esc_html(get_admin_page_title()) . '</h1>';
-
-        // Get all posts with a '_threejs_model' meta key
-        $args = array(
-            'post_type' => 'post',
-            'meta_key' => '_threejs_model',
-            'posts_per_page' => -1
-        );
-        $query = new WP_Query($args);
-
-        // Display the 3D models in a list
-        echo '<h2>3D Models:</h2>';
-        echo '<ul>';
-        while ($query->have_posts()) : $query->the_post();
-            $threejs_model = get_post_meta(get_the_ID(), '_threejs_model', true);
-            echo '<li><a href="' . get_edit_post_link() . '">' . $threejs_model . '</a></li>';
-        endwhile;
-        echo '</ul>';
-
-        wp_reset_postdata();
-
+    
+        echo '<form method="post" action="options.php" enctype="multipart/form-data">';
+        settings_fields('threejs-model-viewer-settings');
+        do_settings_sections('threejs-model-viewer-settings');
+        submit_button();
+        echo '</form>';
+    
         echo '</div>';
     }
-    public function add_meta_boxes() {
-        add_meta_box(
-            'threejs_model_viewer_meta_box',
-            __('ThreeJS Model Viewer', 'threejs-model-viewer'),
-            array($this, 'render_meta_box'),
-            'any',
-            'normal',
-            'high'
-        );
-    }
-    public function render_meta_box($post) {
-        wp_nonce_field('threejs_model_viewer_nonce_action', 'threejs_model_viewer_nonce'); 
+    
+    
+    
+    
 
-        $threejs_model = get_post_meta($post->ID, '_threejs_model', true);
-        $threejs_position = get_post_meta($post->ID, '_threejs_position', true);
-        $threejs_lighting = get_post_meta($post->ID, '_threejs_lighting', true);
-        $threejs_lighting_position = get_post_meta($post->ID, '_threejs_lighting_position', true);
-
-        echo '<label for="threejs_model">3D Model</label>';
-        echo '<input type="text" id="threejs_model" name="threejs_model" value="' . esc_attr($threejs_model) . '" size="25" />';
-        echo '<label for="threejs_position">Model Position</label>';
-        echo '<input type="text" id="threejs_position" name="threejs_position" value="' . esc_attr($threejs_position) . '" size="25" />';
-        echo '<label for="threejs_lighting">Lighting</label>';
-        echo '<select id="threejs_lighting" name="threejs_lighting">';
-        echo '<option value="Ambient" ' . selected($threejs_lighting, 'Ambient', false) . '>Ambient</option>';
-        echo '<option value="Point" ' . selected($threejs_lighting, 'Point', false) . '>Point</option>';
-        echo '</select>';
-        echo '<label for="threejs_lighting_position">Lighting Position</label>';
-        echo '<input type="text" id="threejs_lighting_position" name="threejs_lighting_position" value="' . esc_attr($threejs_lighting_position) . '" size="25" />';
-    }
-    public function save_post($post_id) {
-        if (!isset($_POST['threejs_model_viewer_nonce']) || !wp_verify_nonce($_POST['threejs_model_viewer_nonce'], 'threejs_model_viewer_nonce_action')) {
-            return $post_id;
-        }
-        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-            return $post_id;
-        }
-        if (!current_user_can('edit_post', $post_id)) {
-            return $post_id;
-        }
-        if (isset($_POST['threejs_model'])) {
-            update_post_meta($post_id, '_threejs_model', sanitize_text_field($_POST['threejs_model']));
-        }
-        if (isset($_POST['threejs_position'])) {
-            update_post_meta($post_id, '_threejs_position', sanitize_text_field($_POST['threejs_position']));
-        }
-        if (isset($_POST['threejs_lighting'])) {
-            update_post_meta($post_id, '_threejs_lighting', sanitize_text_field($_POST['threejs_lighting']));
-        }
-        if (isset($_POST['threejs_lighting_position'])) {
-            update_post_meta($post_id, '_threejs_lighting_position', sanitize_text_field($_POST['threejs_lighting_position']));
-        }
-    }
     public function run()
     {
         add_action('admin_menu', array($this, 'add_admin_menu'));
--- threejs-model-viewer.php
+++ threejs-model-viewer.php
@@ -4,38 +4,62 @@
  * Plugin Name: ThreeJS Model Viewer
  * Plugin URI: FILL_IN_LATER
  * Plugin Description: Plugin to add ThreeJS Based Model Loading on WordPress Websites
- * Version: Alpha 0.0.14
+ * Version: Alpha 0.0.26
  * Author: Ryan Chippendale
  * Author URI: FILL_IN_LATER
  * License: GPL2
  */
 
- define('version_number', '0.0.14');
+define('version_number', '0.0.26');
 
- if (!defined('WPINC')) {
+if (!defined('WPINC')) {
     die;
- }
+}
+
 /* Main class */
- class ThreeJS_Model_Viewer {
+class ThreeJS_Model_Viewer {
     protected $loader;
     protected $admin;
     protected $plugin_slug;
     protected $version;
 
-    /**
-     *  Main contrustor setting the plugin version and plugin slug
-     * loading dependancy and admin hook functions
-     * */
     public function __construct() {
         $this->plugin_slug = 'threejs-model-viewer';
         $this->version = version_number;
 
         $this->load_dependencies(); 
         $this->define_admin_hooks();
+        $this->define_shortcodes();
     }
-    /**
-     *  Loads plugin files 
-     * */
+    
+    private function define_shortcodes() {
+        add_shortcode('threejs_model', array($this, 'insert_model'));
+    }
+
+    public function insert_model($atts) {
+        $atts = shortcode_atts(array(
+            'id' => null
+        ), $atts, 'threejs_model');
+        
+        if (empty($atts['id'])) {
+            return '';
+        }
+        
+        $model_id = $atts['id'];
+        
+        // Load the 3D model with the given ID
+        $model = $this->load_model($model_id);
+        
+        if (!$model) {
+            return '';
+        }
+        
+        // Render the HTML representation of the model
+        $html = '<div class="threejs-model">' . $model->render() . '</div>';
+        
+        return $html;
+    
+    }
     private function load_dependencies() {
         require_once (plugin_dir_path(__FILE__) . 'includes/class-threejs-model-viewer-loader.php');
         require_once (plugin_dir_path(__FILE__) . 'admin/class-threejs-model-viewer-admin.php');
@@ -43,27 +67,27 @@
         $this->loader = new ThreeJS_Model_Viewer_Loader(); 
         $this->admin = new ThreeJS_Model_Viewer_Admin($this->get_plugin_slug(), version_number);
     }
+
     private function define_admin_hooks() {
         $this->loader->add_action('admin_enqueue_scripts', $this->admin, 'enqueue_styles');
         $this->loader->add_action('admin_enqueue_scripts', $this->admin, 'enqueue_scripts');
-        $this->loader->add_action('add_meta_boxes', $this->admin, 'add_meta_boxes');
-        $this->loader->add_action('save_post', $this->admin, 'save_post');
         $this->loader->add_action('admin_menu', $this->admin, 'add_admin_menu');
     }
+
     public function run() {
         $this->loader->run();
     }
-    // public function get_plugin_version() {
-    //     return $this->version;
-    // }
+
     public function get_plugin_slug() {
         return $this->plugin_slug;
     }
- }
+}
  
- function run_threejs_model_viewer() {
+function run_threejs_model_viewer() {
     $tjs = new ThreeJS_Model_Viewer();
     $tjs->run(); 
- }
- run_threejs_model_viewer();
- ?>
+}
+
+run_threejs_model_viewer();
+
+?>
